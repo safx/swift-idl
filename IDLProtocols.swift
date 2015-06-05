21da52bba3b08349aadadad92d4ce0dbb89327f6
Safx <safxdev@gmail.com>
2015-06-06 00:05:45 +0900

    add protocols

---
+++ IDLProtocols.swift
@@ -0,0 +1,14 @@
+//
+//  Protocols.swift
+//  Swift-idl
+//
+//  Created by Safx Developer on 2015/05/12.
+//  Copyright (c) 2015年 Safx Developers. All rights reserved.
+//
+
+protocol ClassInit {}
+protocol Printable {}
+protocol JSONEncodable {}
+protocol JSONDecodable {}
+protocol URLRequestHelper {}
+protocol EnumStaticInit {}
